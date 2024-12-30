52fc09f14ae8b3aa4ed8eb48e179073f3cdc04c4
Darío <48804489+nulkode@users.noreply.github.com>
2024-12-30 16:31:11 +0100

    fix: don't accept files when canDrop is false also removed dashed border

--- lib/ui/components/drop_overlay.dart
+++ lib/ui/components/drop_overlay.dart
@@ -1,4 +1,3 @@
-import 'package:dotted_border/dotted_border.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:yaffuu/logic/bloc/files.dart';
@@ -17,66 +16,66 @@ class _DropOverlayState extends State<DropOverlay> {
   @override
   Widget build(BuildContext context) {
     return IgnorePointer(
-      child: DropTarget(
-        onDragEntered: (detail) {
-          setState(() {
-            _dragging = true;
-          });
-        },
-        onDragExited: (detail) {
-          setState(() {
-            _dragging = false;
-          });
-        },
-        onDragDone: (detail) {
-          setState(() {
-            _dragging = false;
-          });
+      child: BlocBuilder<FilesBloc, FilesState>(
+        builder: (context, state) {
+          final canDrop = state is AcceptingFilesState;
 
-          if (detail.files.isNotEmpty) {
-            if (detail.files.length > 1) {
-              ScaffoldMessenger.of(context).showSnackBar(
-                const SnackBar(
-                  content: Text('Only one file at a time (for now)'),
-                ),
-              );
+          return DropTarget(
+            onDragEntered: (detail) {
+              setState(() {
+                _dragging = true;
+              });
+            },
+            onDragExited: (detail) {
+              setState(() {
+                _dragging = false;
+              });
+            },
+            onDragDone: (detail) {
+              setState(() {
+                _dragging = false;
+              });
 
-              return;
-            }
+              if (!canDrop) {
+                return;
+              }
 
-            context.read<FilesBloc>().add(SubmitFilesEvent(detail.files.first));
-          }
-        },
-        child: Opacity(
-          opacity: _dragging ? 1.0 : 0.0,
-          child: BlocBuilder<FilesBloc, FilesState>(
-            builder: (context, state) {
-              final canDrop = state is AcceptingFilesState;
+              if (detail.files.isNotEmpty) {
+                if (detail.files.length > 1) {
+                  ScaffoldMessenger.of(context).showSnackBar(
+                    const SnackBar(
+                      content: Text('Only one file at a time (for now)'),
+                    ),
+                  );
 
-              return Container(
+                  return;
+                }
+
+                context
+                    .read<FilesBloc>()
+                    .add(SubmitFilesEvent(detail.files.first));
+              }
+            },
+            child: Opacity(
+              opacity: _dragging ? 1.0 : 0.0,
+              child: Container(
                 decoration: BoxDecoration(
                   color: canDrop
                       ? Theme.of(context).colorScheme.primary.withOpacity(0.5)
                       : Colors.red.withOpacity(0.5),
                 ),
                 padding: const EdgeInsets.all(48.0),
-                child: DottedBorder(
-                  color: Colors.white,
-                  strokeWidth: 32.0,
-                  dashPattern: const [100, 75],
-                  borderType: BorderType.RRect,
-                  child: Center(
-                    child: Icon(
-                      canDrop ? Icons.add : Icons.block,
-                      color: Colors.white,
-                      size: 100.0,
-                    ),
+                child: Center(
+                  child: Icon(
+                    canDrop ? Icons.add : Icons.block,
+                    color: Colors.white,
+                    size: 100.0,
                   ),
                 ),
-              );
-            },
-          ),
-        ),
+              ),
+            ),
+          );
+        },
       ),
     );
   }
